511221c48fe2c92939402ffdbe7fdb03561bc227
ZawNaingOo0 <zawnaingoo.mhs@gmail.com>
2023-05-30 21:44:17 +0630

    testAppComponent

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+ 
+  it('display loading state initially', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+    // Verify that loading message is displayed
+    const loadingMesage = screen.getByText('Wellcome To Fun Quizz');
+    expect(loadingMesage).toBeInTheDocument();
+  });
+
+  it('display the quiz after loading', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+
+    setTimeout(() => {
+      // Verify that the quiz section is rendered
+      const quizSection = screen.queryByText('Question');
+    expect(quizSection).toBeInTheDocument();
+
+    // Verify that the result section is not rendered
+    const resultSection = screen.queryByText('Congratulations!');
+    expect(resultSection).not.toBeInTheDocument();
+    }, 5000);
+  })
+  
 });
